01b8562724eeb5afd8ca281f454fc42872edeaec
KDFX Team <vitalymistrov@gmail.com>
2023-12-03 16:14:20 +0300

    console

--- my_balans.py
+++ my_balans.py
@@ -1,15 +1,16 @@
 class User:
-    def __init__(self, name):
-        self.name = [name]
+    def __init__(self,name):
+        self.name = name
         self.balance = 0
 
     def load_balance(self):
         try:
             with open('balances.txt', 'r' ) as file:
-                for line in file:
-                    user_name = line.strip().split(':')
-                    if user_name == self.name:
-                        self.balance = int(balance)
+                line = file.readline()
+                for lin in line:
+                    name = lin.strip().split(':')
+                    if name[0] == self.name:
+                        self.balance = int(self.balance)
                         break
         except FileNotFoundError:
             pass
@@ -23,23 +24,23 @@ class User:
 
 
 def menu():
-    nam = input("Введите имя: ")
-    user = User(nam)
-
-    user.save_balance()
+    name = input("Введите имя: ")
+    user = User(name)
+    user.load_balance()
     if user.balance == 0:
+       #print(f"Привет, {user.name}! Твой баланс: {user.balance}")
         print("Имя не найдено, создаем нового пользователя.")
-
     else:
         print(f"Привет, {user.name}! Твой текущий баланс: {user.balance}")
 
     choice = input("Хотите пополнить баланс? (да/нет): ")
+
     if choice.lower() == 'да':
         amount = int(input("Введите сумму пополнения: "))
         user.up_balance(amount)
         user.save_balance()
         print(f"Баланс успешно пополнен. Твой текущий баланс: {user.balance}")
-
+menu()
 
 
 
